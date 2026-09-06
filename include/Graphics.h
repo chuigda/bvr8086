f64f433ca6c3599ecd9b500483ac00baacc60a1c
Chuigda Whitegive <chuigda@doki7.club>
2026-09-06 21:03:41 +0800

    ~

--- include/Graphics.h
+++ include/Graphics.h
@@ -1,4 +1,32 @@
 #ifndef BVR_GRAPHICS_H
 #define BVR_GRAPHICS_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    uint16_t nWidth;
+    uint16_t nHeight;
+    uint8_t bColorDepth;
+    uint8_t bModeID;
+} BvrGraphicsMode;
+
+typedef struct {
+    BvrGraphicsMode mode;
+    uint16_t buffer[0];
+} BvrFramebuffer;
+
+int BvrDetectGraphics(BvrGraphicsMode *pMode, uint16_t nMode);
+BvrFramebuffer* BvrInitGraphics(BvrGraphicsMode const *pMode);
+void BvrSwapBuffers(BvrFramebuffer const *pFramebuffer);
+void BvrCloseGraphics(void);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
 #endif /* BVR_GRAPHICS_H */
