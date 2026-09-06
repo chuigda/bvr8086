76db2e6589110c80876d970af3dcb97f60b9a3ce
Chuigda Whitegive <chuigda@doki7.club>
2026-09-06 23:24:25 +0800

    ~

--- include/Graphics.h
+++ include/Graphics.h
@@ -26,6 +26,53 @@ BvrFramebuffer* BvrInitGraphics(BvrGraphicsMode const *pMode);
 void BvrSwapBuffers(BvrFramebuffer const *pFramebuffer);
 void BvrCloseGraphics(void);
 
+typedef bool (*BvrVertexShader)(void const *pUniform, void const *pVertex,
+                                float (*pPosition)[4], void *pPixelData);
+typedef void (*BvrInterpolate)(void const (*pPixelData)[3],
+                               float const (*pBarycentric)[3],
+                               void *pInterpolatedData);
+typedef bool (*BvrFragmentShader)(void const *pUniform,
+                                  float const (*pPosition)[4],
+                                  void const* pPixelData,
+                                  float (*pColor)[3]);
+
+typedef struct {
+    BvrVertexShader vertexShader;
+    BvrInterpolate interpolate;
+    BvrFragmentShader fragmentShader;
+    void *pUniform;
+
+    uint16_t vertexStride;
+    uint16_t pixelDataStride;
+    uint32_t pixelDataBuffer[0];
+} BvrGraphicsPipeline;
+
+BvrGraphicsPipeline BvrCreatePipeline(BvrVertexShader vertexShader,
+                                      BvrInterpolate interpolate,
+                                      BvrFragmentShader fragmentShader,
+                                      void *pUniform,
+                                      uint16_t vertexStride,
+                                      uint16_t pixelDataStride);
+
+typedef struct {
+    BvrGraphicsMode mode;
+    float depthBuffer[0];
+} BvrDepthBuffer;
+
+BvrDepthBuffer *BvrCreateDepthBuffer(BvrFramebuffer const *pFramebuffer);
+
+void BvrDraw3D(BvrFramebuffer *pFramebuffer,
+               BvrDepthBuffer *pDepthBuffer,
+               BvrGraphicsPipeline const *pPipeline,
+               void const *pVertices, void *pVertexDataStorage,
+               uint32_t nVertices);
+
+void BvrDraw3DIndirect(BvrFramebuffer *pFramebuffer,
+                       BvrDepthBuffer *pDepthBuffer,
+                       BvrGraphicsPipeline const *pPipeline,
+                       void const *pVertices, uint16_t const *pIndices,
+                       void *pVertexDataStorage, uint32_t nIndices);
+
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
